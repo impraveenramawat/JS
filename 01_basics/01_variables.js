2728ba5c4dd28999547af4370658f785713c5c1b
impraveenramawat <99232899+impraveenramawat@users.noreply.github.com>
2023-10-18 08:11:01 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144453;
+let accountEmail = "praveen@gmail.com";
+var accountPassword = 12345678
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+prefer not to use var in JS
+because of block scope and function scope
+*/
